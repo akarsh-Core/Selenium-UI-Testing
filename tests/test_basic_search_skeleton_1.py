1cf7446e5768ae899a9d3dcbae3afb21e7b6f927
Coredge.io <coredge.io@Coredgeios-MacBook-Pro.local>
2023-08-15 23:59:15 +0530

    basic selenium features and test in pytest

---
+++ tests/test_basic_search_skeleton_1.py
@@ -0,0 +1,23 @@
+"""
+These test shows the skeleton for a basic DuckDuckGo searches test case.
+"""
+
+
+def test_basic_duckduckgo_search(browser):
+    # Given the DuckDuckGo home page is displayed
+    # TODO
+
+    # When the user searches for "tiger"
+    # TODO
+
+    # Then the search result title contains "tiger"
+    # TODO
+
+    # And the search result query is "tiger"
+    # TODO
+
+    # And the search result links pertain to "tiger"
+    # TODO
+
+    # we want this to fail as this test is incomplete
+    raise Exception("Incomplete Test")
